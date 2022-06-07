a194c2ec49f76f193a41700ac24eabfd02ab35cd
Pingu <jesper@acorneroftheweb.com>
2022-06-07 09:45:22 +0200

    Base config added, except for config.el

---
+++ custom.el
@@ -0,0 +1,54 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(LaTeX-item-indent 0)
+ '(custom-safe-themes
+   '("5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" "4f1d2476c290eaa5d9ab9d13b60f2c0f1c8fa7703596fa91b235db7f99a9441b" "e2c926ced58e48afc87f4415af9b7f7b58e62ec792659fcb626e8cba674d2065" "353ffc8e6b53a91ac87b7e86bebc6796877a0b76ddfc15793e4d7880976132ae" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "1436985fac77baf06193993d88fa7d6b358ad7d600c1e52d12e64a2f07f07176" "e990fd36b40108405468d7d338feff98420ab967d824115a9a92668bad949af9" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "20bee849714632f54b373922e93eff0b6e5e093f30a8102045aca963c9823ebc" default))
+ '(custom-set-variables '(haskell-process-type 'stack-ghci))
+ '(display-time-24hr-format t)
+ '(display-time-default-load-average nil)
+ '(display-time-format nil)
+ '(display-time-mode t)
+ '(haskell-interactive-popup-errors nil)
+ '(haskell-interactive-prompt "λ ")
+ '(haskell-process-args-ghci '("ghci"))
+ '(haskell-process-args-stack-ghci '("--ghci-options=-ferror-spans"))
+ '(haskell-process-path-ghci "stack")
+ '(org-agenda-files '("~/testing.org"))
+ '(org-highlight-latex-and-related '(native script entities))
+ '(org-latex-default-packages-alist
+   '(("AUTO" "inputenc" t
+      ("pdflatex"))
+     ("T1" "fontenc" t
+      ("pdflatex"))
+     ("" "graphicx" t nil)
+     ("" "longtable" nil nil)
+     ("" "wrapfig" nil nil)
+     ("" "rotating" nil nil)
+     ("normalem" "ulem" t nil)
+     ("" "amsmath" t nil)
+     ("" "amssymb" t nil)
+     ("" "capt-of" nil nil)
+     ("" "hyperref" nil nil)
+     ("" "esint" t nil)))
+ '(org-latex-prefer-user-labels t)
+ '(package-selected-packages
+   '(srefactor elisp-format clang-format mpv dracula-theme mu4e-overview mu4e-views wordel dante vhdl-tools flycheck-haskell lsp-haskell pdf-tools sublimity lsp-java tree-sitter-langs tree-sitter fira-code-mode ##))
+ '(safe-local-variable-values '((dante-methods new-build)))
+ '(warning-suppress-types
+   '((after-save-hook)
+     (after-save-hook)
+     (after-save-hook)
+     (after-save-hook)
+     (after-save-hook)
+     (after-save-hook)
+     (doom-first-file-hook)
+     (doom-init-ui-hook))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
