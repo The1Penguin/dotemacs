1d42f1bc284e02a6348ce59bf5fca925da29abdd
pingu <nor@acorneroftheweb.com>
2023-11-01 10:38:41 +0100

    Let there be a bnf

--- packages.el
+++ packages.el
@@ -19,6 +19,7 @@
 (package! hcl-mode)
 (package! fancy-battery)
 (package! catppuccin-theme)
+(package! bnf-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
