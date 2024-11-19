2774c58f783b525033b75278e8eca6b978ff77be
pingu <nor@acorneroftheweb.com>
2024-11-19 14:45:11 +0100

    baaa

--- init.el
+++ init.el
@@ -101,6 +101,7 @@
        ;;prodigy           ; FIXME managing external services & code builders
        rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
+       tree-sitter         ; syntax and parsing, sitting in a tree...
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
