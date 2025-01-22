242699699d92366b78542d290cda081ec8cd0ff1
pingu <nor@acorneroftheweb.com>
2025-01-22 14:21:49 +0100

    I am a bit sad

--- config.el
+++ config.el
@@ -76,5 +76,4 @@
 (add-hook 'evil-insert-state-exit-hook  (lambda () (set-input-method nil)))
 
 (setq company-idle-delay nil)
-
-(setq tex-compile-commands ("latexmk -pdf %r"))
+(setq TeX-command-default "laTeXMk")
